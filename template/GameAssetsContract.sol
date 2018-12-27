aa527fc40a9738ea0bbb5b0d5ed418531e028182
Nikolay Toporkov <toporkov.kolya96@gmail.com>
2018-12-27 13:23:57 +0300

    add templating

--- template/GameAssetsContract.sol
+++ template/GameAssetsContract.sol
@@ -10,8 +10,7 @@ import "./ERC721/ERC721Burnable.sol";
  *      See https://github.com/OpenZeppelin/openzeppelin-solidity
  */
 contract GameAssetsContract is ERC721Full, ERC721Mintable, ERC721Burnable {
-    constructor() ERC721Full("MyWish Token", "WISH") public {
-        _addMinter(0x8ffff2c69f000c790809f6b8f9abfcbaab46b322);
+    constructor() ERC721Full("D_NAME", "D_SYMBOL") public {
+        _addMinter(D_OWNER);
     }
 }
-
